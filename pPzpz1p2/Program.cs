1d87cef875c870006a5280ab56b0e1eb927247e2
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 16:04:14 +0100

    Add files via upload

---
+++ pPzpz1p2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pPzpz1p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = ' ';
+            int n, nMax = 0;
+            Console.WriteLine("Unesite rečenicu: ");
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                n = 0;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        n++;
+                    }
+                }
+                if (n > nMax)
+                {
+                    c = s[i];
+                    nMax = n;
+                }
+            }
+            Console.WriteLine(s+" -> "+c+", "+nMax+" puta.");
+            Console.ReadKey();
+        }
+    }
+}
